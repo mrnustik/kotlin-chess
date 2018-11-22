0d17edb7afdc0c8b14ecc4dcb5c6ebc203a291e0
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-11-22 18:14:01 +0100

    Use NullPiece object in Position class

--- kotlin-chess-common/src/main/kotlin/net/mrnustik/chess/Position.kt
+++ kotlin-chess-common/src/main/kotlin/net/mrnustik/chess/Position.kt
@@ -1,5 +1,6 @@
 package net.mrnustik.chess
 
+import net.mrnustik.chess.pieces.NullPiece
 import net.mrnustik.chess.pieces.Piece
 
 fun Int.isOdd() : Boolean = this % 2 == 0
@@ -7,7 +8,7 @@ fun Int.isEven() : Boolean = this % 2 != 0
 
 class Position (val x: Int,val y: Int){
 
-    var piece: Piece? = null
+    var piece: Piece = NullPiece()
 
     val color: Color
         get() = if ((x.isOdd() && y.isOdd()) || (x.isEven() && y.isEven())) {
@@ -16,7 +17,7 @@ class Position (val x: Int,val y: Int){
             Color.WHITE
         }
 
-    fun isEmpty(): Boolean = piece == null
+    fun isEmpty(): Boolean = piece is NullPiece
 
     override fun toString(): String {
         return "[$x,$y]"
