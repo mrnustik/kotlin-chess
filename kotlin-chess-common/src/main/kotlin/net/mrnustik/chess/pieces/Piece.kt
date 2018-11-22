df1801ad345fedb4b89e3a83f1d43f18b96b46e5
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-11-22 17:05:59 +0100

    Add color property to Piece class

--- kotlin-chess-common/src/main/kotlin/net/mrnustik/chess/pieces/Piece.kt
+++ kotlin-chess-common/src/main/kotlin/net/mrnustik/chess/pieces/Piece.kt
@@ -1,8 +1,9 @@
 package net.mrnustik.chess.pieces
 
+import net.mrnustik.chess.Color
 import net.mrnustik.chess.moves.RelativeMove
 
-abstract class Piece {
+abstract class Piece (val color: Color){
     abstract val character: Char
 
     abstract val attackMoves: Set<RelativeMove>
