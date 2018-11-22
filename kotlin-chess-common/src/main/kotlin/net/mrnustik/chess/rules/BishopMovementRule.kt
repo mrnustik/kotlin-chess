d4ab4ee820d293219150fc886f14f2f1f15e60e2
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-11-22 23:13:32 +0100

    Add bishop movement rule
    Signed-off-by: Michal Mrnuštík <michal.mrnustik@outlook.com>

---
+++ kotlin-chess-common/src/main/kotlin/net/mrnustik/chess/rules/BishopMovementRule.kt
@@ -0,0 +1,67 @@
+package net.mrnustik.chess.rules
+
+import net.mrnustik.chess.Position
+import net.mrnustik.chess.board.Board
+import net.mrnustik.chess.moves.Move
+
+class BishopMovementRule : BaseRule() {
+    override fun getValidMoves(board: Board, currentPosition: Position): Set<Move> {
+        val moves = mutableSetOf<Move>()
+        moves.addTopLeftQuadrantMoves(board, currentPosition)
+        moves.addTopRightQuadrantMoves(board, currentPosition)
+        moves.addBottomLeftQuadrantMoves(board, currentPosition)
+        moves.addBottomRightQuadrantMoves(board, currentPosition)
+        return moves
+    }
+
+    private fun MutableSet<Move>.addTopLeftQuadrantMoves(board: Board, position: Position) {
+        val changeX = { x: Int -> x - 1 }
+        val changeY = { y: Int -> y + 1 }
+        this.addAvailableMovesMoves(board, position, changeX, changeY)
+    }
+
+
+    private fun MutableSet<Move>.addTopRightQuadrantMoves(board: Board, position: Position) {
+        val changeX = { x: Int -> x + 1 }
+        val changeY = { y: Int -> y + 1 }
+        this.addAvailableMovesMoves(board, position, changeX, changeY)
+    }
+
+    private fun MutableSet<Move>.addBottomLeftQuadrantMoves(board: Board, position: Position) {
+        val changeX = { x: Int -> x - 1 }
+        val changeY = { y: Int -> y - 1 }
+        this.addAvailableMovesMoves(board, position, changeX, changeY)
+    }
+
+    private fun MutableSet<Move>.addBottomRightQuadrantMoves(board: Board, position: Position) {
+        val changeX = { x: Int -> x + 1 }
+        val changeY = { y: Int -> y - 1 }
+        this.addAvailableMovesMoves(board, position, changeX, changeY)
+    }
+
+    private fun MutableSet<Move>.addAvailableMovesMoves(board: Board, position: Position, changeX : (Int) -> Int, changeY : (Int) -> Int) {
+        var x = changeX(position.x)
+        var y = changeY(position.y)
+        while(isWithinBounds(x, y))
+        {
+            val movePosition= board.positions[y][x]
+            if (movePosition.isEmpty()) {
+                this.add(Move(position, movePosition))
+            } else {
+                if(isAttackable(position, movePosition)) {
+                    this.add(Move(position, movePosition))
+                }
+                break
+            }
+            x = changeX(x)
+            y = changeY(y)
+        }
+    }
+
+}
+
+
+
+
+
+
---
+++ kotlin-chess-common/src/test/kotlin/net/mrnustik/chess/rules/BishopMovementRuleTests.kt
@@ -0,0 +1,82 @@
+package net.mrnustik.chess.rules
+
+import net.mrnustik.chess.Color
+import net.mrnustik.chess.board.Board
+import net.mrnustik.chess.board.factory.StandardBoardFactory
+import net.mrnustik.chess.moves.Move
+import net.mrnustik.chess.pieces.NullPiece
+import net.mrnustik.chess.pieces.factory.StandardPieceFactory
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class BishopMovementRuleTests {
+    val pieceFactory = StandardPieceFactory()
+    val boardFactory = StandardBoardFactory(pieceFactory)
+
+    @Test
+    fun getMoves_whenBlocked_noMovesReturned(){
+        //Arrange
+        val board = Board()
+        board.positions[3][3].piece = pieceFactory.createBishop(Color.WHITE)
+        board.positions[2][2].piece = pieceFactory.createPawn(Color.WHITE)
+        board.positions[4][4].piece = pieceFactory.createPawn(Color.WHITE)
+        board.positions[4][2].piece = pieceFactory.createPawn(Color.WHITE)
+        board.positions[2][4].piece = pieceFactory.createPawn(Color.WHITE)
+        val rule = BishopMovementRule()
+
+        //Act
+        val moves = rule.getValidMoves(board, board.positions[3][3])
+
+        //Assert
+        assertEquals(0, moves.size)
+    }
+
+    @Test
+    fun getMoves_whenMovesAvailableInTopRightQuadrant_availableMovesReturned(){
+        //Arrange
+        val board = boardFactory.createBoard()
+        board.positions[1][3].piece = NullPiece()
+        val rule = BishopMovementRule()
+
+        //Act
+        val moves = rule.getValidMoves(board, board.positions[0][2])
+
+        //Assert
+        assertEquals(5, moves.size)
+    }
+
+    @Test
+    fun getMoves_whenMovesAvailableInAllQuadrants_availableMovesReturned(){
+        //Arrange
+        val board = Board()
+        board.positions[4][4].piece = pieceFactory.createBishop(Color.BLACK)
+        val rule = BishopMovementRule()
+
+        //Act
+        val moves = rule.getValidMoves(board, board.positions[4][4])
+
+        //Assert
+        assertEquals(13, moves.size)
+    }
+
+    @Test
+    fun getMoves_whenEnemyCanBeTaken_attackMoveReturned(){
+        //Arrange
+        val board = Board()
+        board.positions[3][3].piece = pieceFactory.createBishop(Color.WHITE)
+        board.positions[2][2].piece = pieceFactory.createPawn(Color.WHITE)
+        board.positions[4][4].piece = pieceFactory.createPawn(Color.BLACK)
+        board.positions[4][2].piece = pieceFactory.createPawn(Color.WHITE)
+        board.positions[2][4].piece = pieceFactory.createPawn(Color.WHITE)
+        val rule = BishopMovementRule()
+
+        //Act
+        val moves = rule.getValidMoves(board, board.positions[3][3])
+
+        //Assert
+        assertEquals(1, moves.size)
+        assertTrue(moves.contains(Move(board.positions[3][3], board.positions[4][4])))
+    }
+}
