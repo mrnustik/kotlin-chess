9e0684fccc058e3ffd7971b179d6f99acb3791b1
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-11-22 18:12:27 +0100

    Remove unused methods from Piece class

--- kotlin-chess-common/src/main/kotlin/net/mrnustik/chess/pieces/Piece.kt
+++ kotlin-chess-common/src/main/kotlin/net/mrnustik/chess/pieces/Piece.kt
@@ -1,12 +1,8 @@
 package net.mrnustik.chess.pieces
 
 import net.mrnustik.chess.Color
-import net.mrnustik.chess.moves.RelativeMove
 
 abstract class Piece (val color: Color){
     abstract val character: Char
-
-    abstract val attackMoves: Set<RelativeMove>
-    abstract val moves : Set<RelativeMove>
 }
 
